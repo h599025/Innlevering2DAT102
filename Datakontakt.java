1c6bddd90ad5a4109bd739d1666dca7b6a5754da
h599025 <89259607+h599025@users.noreply.github.com>
2022-02-15 12:47:04 +0100

    Add files via upload

---
+++ Datakontakt.java
@@ -0,0 +1,10 @@
+package no.hvl.dat102.oppg2;
+
+public class Datakontakt {
+	
+	private int antallMedlemmer = 0;
+	
+	public Datakontakt(int antallMedlemmer) {
+		this.antallMedlemmer = antallMedlemmer;
+	}
+}
